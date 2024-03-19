03d2bfe9b6bce2da69c81faa5d1fcb94703af1db
MICHELSVETOVBASED <147500401+MICHELSVETOVBASED@users.noreply.github.com>
2024-03-19 17:02:03 +0100

    Yeahman

--- Examplify/Program.cs
+++ Examplify/Program.cs
@@ -5,24 +5,38 @@ using System.Runtime.InteropServices;
 
 namespace Examplify{
     internal class Program{
+        internal class Car {}
+        internal class Truck : Car {}
         public static void Main(string[] args){
-            void Swap(int[] array, int index1, int index2)
-            {
+            void Swap(int[] array, int index1, int index2){
                 array[index1] ^= array[index2];
                 array[index2] ^= array[index1];
                 array[index1] ^= array[index2];
             }
-
-// Пример использования:
-
             int a = 2;
             int b = 3;
-            Console.WriteLine(a^=b);
+            Console.WriteLine(a ^= b);
             Console.WriteLine(b ^= a);
             Console.WriteLine(a ^= b);
+            object o1 = new object();
+            object o2 = new Car();
+            object o3 = new Truck();
+            object o4 = o3;
+            Car car1 = new Car();
+            Car car2 = new Truck();
+            Truck truck1 = new Truck();
+            Car car4 = truck1;
+            Truck truck4 = (Truck)truck1;
+            Truck truck5 = (Truck)car2;
+            Car car6 = (Truck)car2;
+            
 
+
+        }
         public delegate void Action();
-        public delegate void Action<in T>(T obj)
-    }
+        public delegate void Action<in T>(T obj);
+
+        
     }
 }
+
