ea8e772227232b4e4cfe99d186cf19d1aa2b602d
MICHELSVETOVBASED <147500401+MICHELSVETOVBASED@users.noreply.github.com>
2024-04-06 10:07:35 +0200

    +++

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿var bar = new Bar{ Foo = new Foo() };
+var foo = new Foo();
+bar.Foo.SetValue(1);
+foo.SetValue(1);
+Console.WriteLine($"{bar.Foo.Value} {foo.Value}");
+
+public struct Foo{
+    public int Value;
+
+    public void SetValue(int newValue){
+        Value = newValue;
+    }
+}
+
+public class Bar{
+    public Foo Foo{ get; set; }
+}
